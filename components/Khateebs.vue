0f5ab1f5d8ab1c2587100e6b5939f8334fb3d936
Ashfaqur Rahman <odesk.rahman@gmail.com>
2022-12-07 14:41:40 +0600

    Design update

--- components/Khateebs.vue
+++ components/Khateebs.vue
@@ -8,30 +8,37 @@
                 </div>                
             </div>
             <div class="row mt-3">
-                <div class="col-lg-4">
-                    <div class="khateebs__block mb-3 mb-lg-0">
-                        <img src="~assets/images/omar_ockeh.jpg" alt="Omar Ockeh" class="img-fluid d-block mx-auto">
-                        <div class="khateebs__name">
-                            <h3>Sh. Omar Ockeh</h3>
+                <div class="col-lg-10 offset-lg-1">
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <div class="khateebs__block mb-3 mb-lg-0">
+                                <img src="~assets/images/omar_ockeh.jpg" alt="Omar Ockeh" class="img-fluid d-block mx-auto">
+                                <div class="khateebs__name">
+                                    <h3>Sh. Omar Ockeh</h3>
+                                    <span class="khateebs__line"></span>
+                                </div>
+                            </div>
                         </div>
+                        <div class="col-lg-4">
+                            <div class="khateebs__block mb-3 mb-lg-0">
+                                <img src="~assets/images/nabil_omran.jpg" alt="Nabil Omran" class="img-fluid d-block mx-auto">
+                                <div class="khateebs__name">
+                                    <h3>Sh. Dr. Nabil Omran</h3>
+                                    <span class="khateebs__line"></span>
+                                </div>
+                            </div>                    
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="khateebs__block mb-3 mb-lg-0">
+                                <img src="~assets/images/imam_shafeeq.jpg" alt="Imam Shafeeq" class="img-fluid d-block mx-auto">
+                                <div class="khateebs__name">
+                                    <h3>Sh. Dr. Imam Shafeeq</h3>
+                                    <span class="khateebs__line"></span>
+                                </div>
+                            </div>                     
+                        </div>                         
                     </div>
-                </div>
-                <div class="col-lg-4">
-                    <div class="khateebs__block mb-3 mb-lg-0">
-                        <img src="~assets/images/nabil_omran.jpg" alt="Nabil Omran" class="img-fluid d-block mx-auto">
-                        <div class="khateebs__name">
-                            <h3>Sh. Dr. Nabil Omran</h3>
-                        </div>
-                    </div>                    
-                </div>
-                <div class="col-lg-4">
-                    <div class="khateebs__block mb-3 mb-lg-0">
-                        <img src="~assets/images/imam_shafeeq.jpg" alt="Imam Shafeeq" class="img-fluid d-block mx-auto">
-                        <div class="khateebs__name">
-                            <h3>Sh. Dr. Imam Shafeeq</h3>
-                        </div>
-                    </div>                     
-                </div>                                
+                </div>                               
             </div>            
         </div>
     </section>
